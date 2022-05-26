6db6dd36b65c5f45d6b310c0763fb867c13c9200
Sam Creed <sam_creed@hotmail.co.uk>
2022-05-26 22:40:04 +0100

    produce working page with adjustable grid size

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 //jshint esversion:6
 
-let cont = document.getElementById("container");
+let grid = 0; //sets the grid variable
 
-for(var i = 1; i <= 16; i++) {
-  var div = document.createElement("div");
-  div.setAttribute("class", "square");
-  div.setAttribute("id", "div" + i);
-  cont.appendChild(div);
+function gridSize() {   //this is the controlling function, it's called by the button on the page
+  grid = prompt("Enter the size of grid in terms of width (up to 100):");
+  gridFunction();
+  assignSquareSize();
+  squareColor();
 }
+
+let cont = document.getElementById("container"); //the variable that obtains the div with class of container
+
+function gridFunction() {     //this function creates new divs that create the etch-a-sketch grid, class of "square" is assigned to use later
+  for(var i = 1; i <= (grid * grid); i++) {
+    var div = document.createElement("div");
+    div.setAttribute("class", "square");
+    div.setAttribute("id", "div" + i);
+    cont.appendChild(div);
+  }}
 
 var squares = document.getElementsByClassName("square");
 
-for (const square of squares) {
-  square.addEventListener("mouseover", function mouseOver() {
-    square.style.backgroundColor = "purple";
-  });
-}
+function assignSquareSize() {
+  for(const square of squares) {
+    var squareSize = (960 / grid) + 'px';
+    console.log(squareSize);
+    square.style.width = squareSize;
+    square.style.height = squareSize;
+}}
+
+function squareColor() {
+  for (const square of squares) {
+    square.addEventListener("mouseover", function mouseOver() {
+      square.style.backgroundColor = "purple";
+    setTimeout(function() {
+      square.style.backgroundColor = "red";
+    }, 2000);
+    });
+  }}
